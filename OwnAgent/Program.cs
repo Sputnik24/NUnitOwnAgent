c321ab89e675f8656259f0ff0e08c730ef6a4909
Daniel Pötzsch <register@dpoetzsch.de>
2024-09-20 08:16:36 +0200

    Initial commit

---
+++ OwnAgent/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml;
+using NUnit.Engine;
+
+try {
+    ITestEngine testEngine = TestEngineActivator.CreateInstance();
+    TestPackage package = new TestPackage("../../../../Tests/bin/Debug/net8.0/Tests.dll");
+    ITestRunner runner = testEngine.GetRunner(package);
+    XmlNode? explorer = runner.Explore(TestFilter.Empty);
+    Console.WriteLine(explorer.OuterXml);
+}
+catch (Exception e) {
+    Console.WriteLine(e);
+}
+
+Console.ReadKey();
---
+++ Tests/Tests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace Tests;
+
+public class Tests {
+    [Test]
+    public void Test1() {
+        Assert.Pass();
+    }
+}
