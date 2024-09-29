215acc0c4634850009af36e40b98675fb6cc33dd
Daniel Pötzsch <daniel.poetzsch@mercedes-benz.com>
2024-09-29 09:51:20 +0200

    Add various ways to provide test file path

--- OwnAgent/Program.cs
+++ OwnAgent/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System.Xml;
 using NUnit.Engine;
 
+//string testFile = "../../../../Tests/bin/Debug/net8.0/Tests.dll"; //File.Exists true, but FileNotFound on runner.Explore
+//string testFile = "../../../../../../../../Tests/bin/Debug/net8.0/Tests.dll"; //File.Exists false but Explore is working
+string testFile = "D:/dev/repos/NUnitOwnAgent/Tests/bin/Debug/net8.0/Tests.dll"; //File.Exists true, Explore is not working
+//string testFile = "Tests.dll"; //Works when WorkDirectory is set
+Console.WriteLine($"File exists: {File.Exists(testFile)}");
+
 try {
     ITestEngine testEngine = TestEngineActivator.CreateInstance();
-    TestPackage package = new TestPackage("../../../../Tests/bin/Debug/net8.0/Tests.dll");
+    //testEngine.WorkDirectory = "D:\\dev\\repos\\NUnitOwnAgent\\Tests\\bin\\Debug\\net8.0";
+    TestPackage package = new TestPackage(testFile);
     ITestRunner runner = testEngine.GetRunner(package);
     XmlNode? explorer = runner.Explore(TestFilter.Empty);
     Console.WriteLine(explorer.OuterXml);
